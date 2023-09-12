a0c8ab2d56135ac40bce0f3c835dd0612cbc13e5
Daulet <daulet@qit.kz>
2023-09-12 16:23:04 +0600

    main

--- main..py
+++ main..py
@@ -1,72 +1,73 @@
-from docx import Document
 import telebot
-import os
+from docx import Document
+import io
 
-# Инициализация
-token = "6654160154:AAG0Ylq41itMfeMj5HxLZW699DKpnKQhCKU"
-bot = telebot.TeleBot(token)
-cabinet_schedule_dict = {}
-group_schedule_dict = {}
+bot = telebot.TeleBot('6654160154:AAG0Ylq41itMfeMj5HxLZW699DKpnKQhCKU')
 
-def extract_data_from_docx(file_info):
-    global cabinet_schedule_dict, group_schedule_dict
-    file_data = bot.download_file(file_info.file_path)
+schedule_dict = {}
 
-    # Сохранение данных файла во временный файл
-    temp_file = "temp_schedule.docx"
-    with open(temp_file, "wb") as f:
-        f.write(file_data)
-    
-    doc = Document(temp_file)
-    os.remove(temp_file)  # Удаляем временный файл после обработки
+def improved_parse_schedule(docx_file):
+    doc = Document(docx_file)
+    time_intervals = [cell.text.strip() for cell in doc.tables[0].rows[0].cells][1::2]
+    schedule = {}
 
     for table in doc.tables:
         for row in table.rows:
-            cabinet = row.cells[0].text.strip().lower()
-            if not cabinet:  # Пропускаем строки без кабинета
-                continue
+            cells = [cell.text.strip() for cell in row.cells]
+            room = cells.pop(0)
 
-            for i, cell in enumerate(row.cells[1:-1:2], start=1):  # Обновлено, чтобы учесть все столбцы и добавить индексацию
-                teacher = cell.text.strip().lower()
-                group = row.cells[i*2].text.strip().lower()
+            # Process data in pairs (Teacher, Group)
+            for i in range(0, len(cells) - 1, 2):  # We subtract 1 to avoid index out of range
+                teachers = cells[i].split("\n") if i < len(cells) else []
+                groups = cells[i+1].split("\n") if (i+1) < len(cells) else []
+                
+                for teacher, group in zip(teachers, groups):
+                    if not teacher or not group:
+                        continue
 
-                # Заполнение словаря для кабинетов
-                if cabinet not in cabinet_schedule_dict:
-                    cabinet_schedule_dict[cabinet] = []
-                cabinet_schedule_dict[cabinet].append((i, teacher, group))  # Добавлен номер пары
+                    if group not in schedule:
+                        schedule[group] = {}
+                    
+                    interval_index = i // 2
+                    current_interval = time_intervals[interval_index]
 
-                # Заполнение словаря для групп
-                if group not in group_schedule_dict:
-                    group_schedule_dict[group] = []
-                group_schedule_dict[group].append((i, teacher, cabinet))  # Добавлен номер пары
+                    schedule[group][current_interval] = {'room': room, 'teacher': teacher}
+
+    return schedule
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Введите название вашей группы, чтобы узнать расписание на сегодня. Или загрузите файл с расписанием для обновления.")
 
 @bot.message_handler(content_types=['document'])
 def handle_docs(message):
-    try:
+    global schedule_dict
+    if message.document.mime_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
         file_info = bot.get_file(message.document.file_id)
-        extract_data_from_docx(file_info)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with io.BytesIO(downloaded_file) as docx_file:
+            schedule_dict = improved_parse_schedule(docx_file)
         bot.reply_to(message, "Расписание успешно обновлено!")
-    except Exception as e:
-        bot.reply_to(message, f"Ошибка при обработке файла: {str(e)}.")
-
-@bot.message_handler(func=lambda message: True)
-def send_schedule(message):
-    query = message.text.strip().lower()
-    response = []
-
-    # Поиск по кабинетам
-    if query in cabinet_schedule_dict:
-        for pair_num, teacher, group in cabinet_schedule_dict[query]:
-            response.append(f"{query} - {pair_num}-я пара: {teacher} ({group})")
-    # Поиск по группам
-    elif query in group_schedule_dict:
-        for pair_num, teacher, cabinet in group_schedule_dict[query]:
-            response.append(f"{cabinet} - {pair_num}-я пара: {teacher} ({query})")
     else:
-        response.append(f"Расписание для {query} отсутствует.")
+        bot.reply_to(message, "Пожалуйста, загрузите документ в формате .docx.")
 
-    bot.reply_to(message, "\n".join(response))
+@bot.message_handler(func=lambda m: True)
+def send_schedule(message):
+    group = message.text
+    if group in schedule_dict:
+        schedule_for_group = schedule_dict[group]
+        response = "Расписание для группы {}:".format(group)
+        for time, details in schedule_for_group.items():
+            teacher_info = details["teacher"] if details["teacher"] else "Преподаватель: Не указан"
+            response += "\n{} - Аудитория: {} - {}".format(time, details["room"], teacher_info)
+    else:
+        response = "Извините, расписание для группы {} не найдено.".format(group)
+    bot.reply_to(message, response)
 
 bot.polling()
+
+
+
+
 
 
